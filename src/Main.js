815e6d2eb3ebbde44d35976fca04deaf336a8104
musicer <musicer@umich.edu>
2022-05-19 21:25:49 -0400

    add navbar

--- src/Main.js
+++ src/Main.js
@@ -13,27 +13,38 @@ function Main() {
           </div>
         </header>
       </div>
-      <div className="main-navbar-div">
-        <p>Navbar</p>
-      </div>
-      <div className="main-summary-div">
-        <h2>Summary</h2>
+      {/* <div className="main-navbar-container-div"> */}
+        <div className="main-navbar-div">
+          <h3 className="navbar-item-first"><a href="#summary">Summary</a></h3> 
+          <h3 className="navbar-item"><a href="#interviews">Interviews</a></h3>
+          <h3 className="navbar-item"><a href="#observations">Observations</a></h3>
+          <h3 className="navbar-item"><a href="#methodology">Methodology</a></h3>
+          <h3 className="navbar-item"><a href="#disclaimer">Disclaimer</a></h3>
+          <h3 className="navbar-item"><a href="#contact">Contact</a></h3>
+        </div>
+      {/* </div> */}
+      <div id="summary" className="main-summary-div">
+        <h2 className="section-title">Summary</h2>
         <p>Text goes here.</p>
       </div>
-      <div className="main-interviews-div">
-        <h2>Interviews</h2>
+      <div id="interviews" className="main-interviews-div">
+        <h2 className="section-title">Interviews</h2>
         <p>Links to individual interviews go here. This will be a div of components.</p>
       </div>
-      <div className="main-statistics-div">
-        <h2>Statistics</h2>
+      <div id="observations" className="main-observations-div">
+        <h2 className="section-title">Observations</h2>
         <p>Text goes here, probably more later.</p>
       </div>
-      <div className="main-methodology-div">
-        <h2>Methodology</h2>
+      <div id="methodology" className="main-methodology-div">
+        <h2 className="section-title">Methodology</h2>
         <p>Text goes here.</p>
       </div>
-      <div className="main-contact-div">
-        <h2>Contact</h2>
+      <div id="disclaimers" className="main-disclaimer-div">
+        <h2 className="section-title">Disclaimers</h2>
+        <p>Text goes here.</p>
+      </div>
+      <div id="contact" className="main-contact-div">
+        <h2 className="section-title">Contact</h2>
         <p>For the time being I will remain anonymous. If you have questions, feel free to contact me at thecourtmusician@gmail.com.</p>
       </div>
     </div>
